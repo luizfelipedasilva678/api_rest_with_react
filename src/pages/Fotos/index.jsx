3321b735f897b159cdfbd82f42ec51c14f171bab
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-06-26 18:35:01 -0300

    register photos

--- src/pages/Fotos/index.jsx
+++ src/pages/Fotos/index.jsx
@@ -1,10 +1,84 @@
 import React from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../style/GlobalStyles';
+import Loading from '../../components/Loading';
+import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
 
-export default function Fotos() {
+export default function Fotos({ match }) {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [foto, setFoto] = React.useState('');
+  const id = get(match, 'params.id', '');
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/alunos/${id}`);
+        setFoto(get(data, 'Fotos[0].url', ''));
+        setIsLoading(false);
+      } catch {
+        toast.error('Erro ao obter imagem');
+        setIsLoading(false);
+        history.push('/');
+      }
+    };
+
+    getData();
+  }, [id]);
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    const fotoUrl = URL.createObjectURL(file);
+
+    setFoto(fotoUrl);
+
+    const formData = new FormData();
+
+    formData.append('arquivo', file);
+    formData.append('aluno_id', id);
+
+    try {
+      setIsLoading(true);
+      await axios.post('/fotos/', formData, {
+        headers: {
+          'Cotent-Type': 'multipart/form-data',
+        },
+      });
+
+      toast.success('Foto enviada');
+
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      const { status } = get(err, 'response', '');
+      toast.error('Erro ao enviar foto');
+
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
   return (
     <Container>
-      <h1>Fotos</h1>
+      <Loading isLoading={isLoading} />
+      <Title>Fotos</Title>
+
+      <Form>
+        <label htmlFor="foto">
+          {foto ? <img src={foto} alt="Foto" /> : 'Selecionar'}
+          <input type="file" id="foto" onChange={handleChange} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Fotos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
